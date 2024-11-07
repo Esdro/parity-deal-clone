46026be029d38b052bbe34b9abfa488c285d7a4e
Esdras Onionkiton <eonionkiton@gmail.com>
2024-11-07 23:40:49 +0100

    prepare build v3

--- src/app/dashboard/products/[productId]/edit/page.tsx
+++ src/app/dashboard/products/[productId]/edit/page.tsx
@@ -13,6 +13,7 @@ import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
 import {InfoIcon} from "lucide-react";
 import {canCustomizeBanner, canRemoveBranding} from "@/server/permissions";
 import ProductCustomizationForm from "@/app/dashboard/_components/ProductCustomizationForm";
+import CountryTabsContent from "@/app/dashboard/products/[productId]/edit/_components/country-tabs-content";
 
 // Pas bien de le faire dans la nouvelle version 15 de Next.js
 /*
@@ -85,45 +86,7 @@ function DetailsTabsContent({product}: { product: typeof ProductTable.$inferInse
 }
 
 
-/**
- * CountryTabsContent
- * @param productId
- * @param userId
- * @constructor
- */
-export async function CountryTabsContent({productId, userId}: { productId: string, userId: string }) {
-
-    const countryGroups = await getProductCountryGroups({productId, userId});
-
-
-    // console.log(countryGroups);
-    /* [
-         {id: "1", name: "Country Group 1", recommendedDiscountPercentage: 10, countries: [{name: "France", code: "FR"}, {name: "Germany", code: "DE"},{name: "Italy", code: "IT"} ], discount: {coupon: "EUROPE10", discountPercentage: 0.1}},
-         {id: "2", name: "Country Group 2", recommendedDiscountPercentage: 30, countries: [ {name: "Bénin", code: "BJ"}, {name: "Cameroon", code: "CM"}, {name: "Algérie", code: "DZ"} ], discount: {coupon: "AFRO30", discountPercentage: 0.2}},
-         {id: "3", name: "Country Group 3", recommendedDiscountPercentage: 20, countries: [{name: "India", code: "IN"}, {name: "China", code: "CN"}], discount: {coupon: "ASIA20", discountPercentage: 0.3}},
-     ]*/
-
-    return (
-        <Card>
-            <CardHeader>
-                <CardTitle className='text-2xl'>Country Discounts</CardTitle>
-                <Alert  className='!my-4 flex gap-8 items-center justify-between '>
-                    <InfoIcon className='size-6 text-red-600 '/>
-                    <div>
-                        <AlertTitle>Country Groups Discount</AlertTitle>
-                        <AlertDescription> Leave the discount field blank if you do not want to display deals for specific parity group </AlertDescription>
-                    </div>
-                </Alert>
-            </CardHeader>
-            <CardContent>
-                <CountryGroupsDiscountForm productId={productId} countryGroups={countryGroups}/>
-            </CardContent>
-        </Card>
-    )
-}
-
-
-export async function CustomizationTabsContent({product, userId}: {
+ async function CustomizationTabsContent({product, userId}: {
     product: typeof ProductTable.$inferInsert,
     userId: string
 }) {
