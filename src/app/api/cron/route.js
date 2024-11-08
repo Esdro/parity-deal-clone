59191687de3ba4693e8d9739296dfc323a29cb58
Esdras Onionkiton <eonionkiton@gmail.com>
2024-11-08 10:57:24 +0100

    prepare build v9

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,5 +1,8 @@
 import {clearFullCache} from "@/lib/cache";
 
 export async function GET() {
+    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+        return res.status(401).end('Unauthorized');
+    }
     return clearFullCache();
 }
